cae898402caf65f983d2738f28dd22e00ce3a094
Lukas Landis <landis.lukas@gmail.com>
2014-05-14 21:15:52 +0200

    convert to android-studio project

--- gen/ch/luklanis/csipsimple/taskerplugin/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ch.luklanis.csipsimple.taskerplugin;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
